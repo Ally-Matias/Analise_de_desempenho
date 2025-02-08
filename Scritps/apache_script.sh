28fb9b4620bf95111d35d8d8ab93a1b80e601cca
Anchieta Albano <111232477+Chiet4@users.noreply.github.com>
2025-02-08 17:48:45 -0300

    update ascript apache

--- Scritps/apache_script.sh
+++ Scritps/apache_script.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-while true; do      echo "$(date '+%Y-%m-%d %H:%M:%S')" >> apache_monitor.log;     ps -eo pid,comm,%cpu,%mem --sort=-%cpu | grep apache2 | awk '{printf "PID: %-6s | Processo: %-10s | CPU: %-5s%% | Memória: %-5s%%\n", $1, $2, $3, $4}' >> apache_monitor.log;     echo "----------------------------------------" >> apache_monitor.log;     sleep 1; done
+#!/bin/bash
+
+while true; do
+        echo "$(date '+%Y-%m-%d %H:%M:%S')" >> apache_monitor.log;
+        ps -eo pid,comm,%cpu --sort=-%cpu | grep apache2 | awk '{printf "PID: %-6s | Processo: %-10s | CPU: %-5s%%\n", $1, $2, $3, $4}' >> apache_monitor.log;
+        free -m | awk 'NR==2{printf "Memória Total: %sMB | Usada: %MB | Livre: %sMB | Cache: %sMB\n", $2, $3, $4, $6}' >> apache_monitor.log
+        echo "----------------------------------------" >> apache_monitor.log;
+        sleep 1; done
